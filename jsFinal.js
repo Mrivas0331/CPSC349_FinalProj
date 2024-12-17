b1497ea56d644958af724e6de95dc41133e44bac
Max Rivas <Rivas.max11@csu.fullerton.edu>
2024-12-17 00:14:35 -0800

    added some last features, will probably add a few more images for color

--- jsFinal.js
+++ jsFinal.js
@@ -2,3 +2,7 @@ function fadeImage() {
     var image = document.getElementById("capy");
     image.style.opacity="1";
 }
+// function fadeImage() {
+//     var fader = document.getElementById("brawl");
+//     image.style.opacity="1";
+// }
